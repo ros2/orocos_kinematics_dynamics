465eb145c054a105bef567b71501cd81b2e36be4
Mikael Arguedas <mikael@osrfoundation.org>
2017-12-17 01:26:13 +0100

    implement logic if sipconfig module doesnt exist

--- python_orocos_kdl/cmake/FindSIP.py
+++ python_orocos_kdl/cmake/FindSIP.py
@@ -5,6 +5,7 @@
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 from __future__ import print_function
 
+import os
 import sys
 
 use_sip = False
@@ -25,11 +26,31 @@ else:
 
 if use_sipconfig:
     sipcfg = sipconfig.Configuration()
-    print("sip_version:%06.0x" % sipcfg.sip_version)
-    print("sip_version_str:%s" % sipcfg.sip_version_str)
-    print("sip_bin:%s" % sipcfg.sip_bin)
-    print("default_sip_dir:%s" % sipcfg.default_sip_dir)
-    print("sip_inc_dir:%s" % sipcfg.sip_inc_dir)
+    print("sip_version:%06.0x" % sipcfg.sip_version, file=sys.stderr)
+    print("sip_version_str:%s" % sipcfg.sip_version_str, file=sys.stderr)
+    print("sip_bin:%s" % sipcfg.sip_bin, file=sys.stderr)
+    print("default_sip_dir:%s" % sipcfg.default_sip_dir, file=sys.stderr)
+    print("sip_inc_dir:%s" % sipcfg.sip_inc_dir, file=sys.stderr)
 else:
+    if sys.platform == "win32":
+        sip_py_lib_dir = os.path.join(sys.prefix, "libs")
+        sip_bin_dir = sys.exec_prefix
+        sip_sip_dir = os.path.join(sys.prefix, "sip")
+    else:
+        import distutils.sysconfig
+        lib_dir = distutils.sysconfig.get_python_lib(plat_specific=1, standard_lib=1)
+        sip_py_lib_dir = os.path.join(lib_dir, "config")
+        sip_bin_dir = os.path.join(sys.exec_prefix, "bin")
+        sip_sip_dir = os.path.join(sys.prefix, "share/sip")
+    sip_inc_dir = distutils.sysconfig.get_python_inc()
+    sip_bin = os.path.join(sip_bin_dir, "sip")
     print("sip_version:%06.0x" % sip.SIP_VERSION)
     print("sip_version_str:%s" % sip.SIP_VERSION_STR)
+    print("sip_bin:%s" % sip_bin)
+    print("default_sip_dir:%s" % sip_sip_dir)
+    print("sip_inc_dir:%s" % sip_inc_dir)
+    print("sip_version:%06.0x" % sip.SIP_VERSION, file=sys.stderr)
+    print("sip_version_str:%s" % sip.SIP_VERSION_STR, file=sys.stderr)
+    print("sip_bin:%s" % sip_bin, file=sys.stderr)
+    print("default_sip_dir:%s" % sip_sip_dir, file=sys.stderr)
+    print("sip_inc_dir:%s" % sip_inc_dir, file=sys.stderr)
